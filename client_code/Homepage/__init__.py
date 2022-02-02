d6bc9481d33eb47007d2295215a47a11beb204f7
www.i.clemenckow2016@mail.ru <www.i.clemenckow2016@mail.ru>
2022-02-02 16:34:42 +0000

    Created ContentManager; Edited Homepage and settings; Deleted Package1

--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -5,14 +5,18 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+from .. import ContentManager
 
 class Homepage(HomepageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    anvil.users.login_with_form()
+    #anvil.users.login_with_form()
+
+  def get_good_icon(self, **event_args):
+    ContentManager.get_good_icon()
 
   def process_checking_click(self, **event_args):
-    self.ai_check_image
-
+    """This method is called when the button is clicked"""
+    pass
 
