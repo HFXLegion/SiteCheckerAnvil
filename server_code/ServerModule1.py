6ba3ab44cb75b083fcb831967fd1b02f23fdc5f8
www.i.clemenckow2016@mail.ru <www.i.clemenckow2016@mail.ru>
2022-02-02 15:34:38 +0000

    Edited Homepage, settings and ServerModule1; Deleted Form1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -10,10 +10,4 @@ def add_user(article_dict):
   current_user = anvil.users.get_user()
   if current_user is not None:
     app_tables.articles.add_row(content=None, title="Main Row", user=current_user, **article_dict)
-    
-@anvil.server.callable
-def get_articles():
-  current_user = anvil.users.get_user()
-  if current_user is not None:
-    return app_tables.articles.search(tables.order_by("created", ascending=True), user=current_user)
 
